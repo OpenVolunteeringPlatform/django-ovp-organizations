6ebe12198f7f1c6e89480916e287453a21d51924
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-11-13 21:58:49 -0200

    Upgrade ovp_core, ovp_users and rest-framework

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
     zip_safe=False,
     install_requires = [
       'Django>=1.10.1,<1.11.0',
-      'djangorestframework>=3.4.7,<3.5.0',
+      'djangorestframework>=3.5.3,<3.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.3.0',
-      'ovp-users>=1.0.9,<2.0.0',
-      'ovp-core>=1.0.1,<2.0.0'
+      'ovp-users>=1.0.10,<2.0.0',
+      'ovp-core>=1.0.3,<2.0.0'
     ]
 )
