5516b125bb00b928d85a044d3df777e1b0004d03
Leonardo Arroyo <contato@leonardoarroyo.com>
2016-12-09 10:27:58 -0200

    Add ".only" restriction to query on migration 0008

--- ovp_organizations/migrations/0008_auto_20161207_1941.py
+++ ovp_organizations/migrations/0008_auto_20161207_1941.py
@@ -7,12 +7,12 @@ from django.db import migrations
 from ovp_organizations.models import Organization
 
 def add_members(apps, schema_editor):
-  for organization in Organization.objects.all():
+  for organization in Organization.objects.only('pk', 'members').all():
     organization.members.add(organization.owner)
 
 
 def remove_members(apps, schema_editor):
-  for organization in Organization.objects.all():
+  for organization in Organization.objects.only('pk', 'members').all():
     organization.members.clear()
 
 
