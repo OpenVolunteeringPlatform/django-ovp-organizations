20f6fc2563fb0b619bb7cb40f6b55910eac93709
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-01-27 17:38:53 -0200

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
       'Django>=1.10.1,<1.11.0',
       'djangorestframework>=3.5.3,<3.6.0',
       'codecov>=2.0.5,<2.1.0',
-      'coverage>=4.2,<4.3.0',
+      'coverage>=4.2,<4.4.0',
       'ovp-users>=1.0.0,<2.0.0',
       'ovp-core>=1.0.0,<2.0.0',
       'ovp-uploads>=1.0.0,<2.0.0'
