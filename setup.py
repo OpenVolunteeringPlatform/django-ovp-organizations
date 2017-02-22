978d067e02335b83d57fff2ccca35595b936674d
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-02-22 19:10:35 -0300

    Upgrade ovp-users and ovp-core

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
       'djangorestframework>=3.5.3,<3.6.0',
       'codecov>=2.0.5,<2.1.0',
       'coverage>=4.2,<4.4.0',
-      'ovp-users>=1.0.0,<2.0.0',
-      'ovp-core>=1.0.0,<2.0.0',
+      'ovp-users>=1.1.3,<2.0.0',
+      'ovp-core>=1.2.4,<2.0.0',
       'ovp-uploads>=1.0.0,<2.0.0'
     ]
 )
